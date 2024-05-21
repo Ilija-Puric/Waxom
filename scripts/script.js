a655f251330ed9fa6805030aa1deca9628543e5f
Ilija-Puric <ilijapuric20@gmail.com>
2024-05-21 19:01:47 +0200

    Added video playback, and current time as the video plays

--- scripts/script.js
+++ scripts/script.js
@@ -47,3 +47,30 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+// Video playing
+
+const videoElement = document.querySelector("video");
+const playPauseButton = document.querySelector(".video-control");
+const videoTimeElement = document.querySelector(".video__time");
+
+playPauseButton.addEventListener("click", () => {
+  playPauseButton.classList.toggle("playing");
+  if (playPauseButton.classList.contains("playing")) {
+    videoElement.play();
+  } else {
+    videoElement.pause();
+  }
+});
+
+videoElement.addEventListener("ended", () => {
+  playPauseButton.classList.remove("playing");
+});
+
+videoElement.addEventListener("timeupdate", () => {
+  const currentTime = videoElement.currentTime;
+  const minutes = Math.floor(currentTime / 60);
+  const seconds = Math.floor(currentTime % 60);
+  const formattedTime = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+  videoTimeElement.textContent = formattedTime;
+});
